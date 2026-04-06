e7d78daaa0cf7e2170d1eb87f1064061c12f3683
sgatz1 <sgatz1@asu.edu>
2026-04-06 02:13:23 -0400

    Update stack.h

--- stack.h
+++ stack.h
@@ -2,7 +2,11 @@
 #ifndef STACK_H
 #define STACK_H
 
-#include "data_structures.h" // ensures ELEMENT and pELEMENT are defined
+#include "data_structures.h" // ensures TAG_VERTEX is defined
+
+// define stack element as VERTEX pointer
+typedef TAG_VERTEX ELEMENT;
+typedef ELEMENT* pELEMENT;
 
 typedef struct TAG_STACK {
     int top;
