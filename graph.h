f2610bdc1d1f509a0cd8f9e4900090326ac32fe7
sgatz1 <sgatz1@asu.edu>
2026-04-06 01:48:27 -0400

    Refactor graph.h for adjacency list structure

--- graph.h
+++ graph.h
@@ -4,16 +4,16 @@
 
 #include "data_structures.h"
 
-// initialize adjacency list for n vertices
-void initializeGraph(pVERTEX* vertices, pNODE** adjList, int n);
+// initialize graph
+void initializeGraph(pVERTEX* vertices, pNODE* adjList, int n);
 
-// add an edge u -> v with weight w
-void addEdge(pNODE** adjList, int u, int v, double w);
+// add edge
+void addEdge(pNODE* adjList, int u, int v, double w);
 
 // dijkstra algorithm
-void dijkstra(pVERTEX* vertices, pNODE** adjList, int startIndex, int n);
+void dijkstra(pVERTEX* vertices, pNODE* adjList, int startIndex, int n);
 
-// print shortest path from start to end
+// print shortest path
 void printShortestPath(pVERTEX* vertices, int startIndex, int endIndex);
 
 #endif // GRAPH_H
