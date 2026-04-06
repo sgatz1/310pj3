9e865203fa1dbb39c46b736bb469307c87361274
sgatz1 <sgatz1@asu.edu>
2026-04-06 02:26:31 -0400

    Refactor util.h to simplify graph functions
    Removed unused graph functions and updated includes.

--- util.h
+++ util.h
@@ -1,18 +1,9 @@
-#pragma once
 #ifndef UTIL_H
 #define UTIL_H
 
-#include "data_structures.h"
+#include "graph.h"
 #include "heap.h"
 
-// Graph functions
-void initializeGraph(pVERTEX* vertices, pNODE** adjList, int n);
-void addEdge(pNODE** adjList, int u, int v, double w);
-
-// Dijkstra
-void dijkstra(pVERTEX* vertices, pNODE** adjList, int startIndex, int n);
-
-// Printing
 void printShortestPath(pVERTEX* vertices, int startIndex, int endIndex);
 
-#endif // UTIL_H
+#endif
