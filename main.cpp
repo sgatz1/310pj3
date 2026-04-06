80cc04237963075b9aba4b8705c4e4fd165c74b4
sgatz1 <sgatz1@asu.edu>
2026-04-06 01:59:18 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
-#include <cfloat>          // <<--- add this for DBL_MAX
+#include <cfloat>           // For DBL_MAX
 #include "data_structures.h"
 #include "graph.h"
-#include "heap.h"
 #include "util.h"
 
-using namespace std;
-
 int main() {
-    int n = 5; // number of vertices
-    pVERTEX* vertices = new pVERTEX[n];
-    pNODE** adjList = new pNODE*[n];
+    // number of vertices
+    int n = 5;
 
-    // initialize vertices
+    // allocate vertices
+    pVERTEX* vertices = new pVERTEX[n];
     for (int i = 0; i < n; i++) {
         vertices[i] = new VERTEX;
         vertices[i]->index = i;
         vertices[i]->color = WHITE;
-        vertices[i]->key = DBL_MAX; // now DBL_MAX is defined
+        vertices[i]->key = DBL_MAX;
         vertices[i]->pi = -1;
         vertices[i]->position = -1;
+    }
+
+    // allocate adjacency list
+    pNODE* adjList = new pNODE[n];
+    for (int i = 0; i < n; i++) {
         adjList[i] = nullptr;
     }
+
+    // initialize graph (optional if you have an init function)
+    initializeGraph(vertices, adjList, n);
 
     // add edges
     addEdge(adjList, 0, 1, 2.0);
@@ -31,18 +36,22 @@ int main() {
     addEdge(adjList, 2, 4, 3.0);
     addEdge(adjList, 3, 4, 1.0);
 
-    // run Dijkstra
+    // run Dijkstra starting from vertex 0
     dijkstra(vertices, adjList, 0, n);
 
-    // print shortest path from 0 to 4
-    printShortestPath(vertices, 0, 4);
+    // print shortest paths from vertex 0 to all others
+    for (int i = 0; i < n; i++) {
+        std::cout << "Path from 0 to " << i << ": ";
+        printShortestPath(vertices, 0, i);
+        std::cout << std::endl;
+    }
 
     // cleanup
     for (int i = 0; i < n; i++) {
         delete vertices[i];
-        NODE* current = adjList[i];
+        pNODE current = adjList[i];
         while (current) {
-            NODE* tmp = current;
+            pNODE tmp = current;
             current = current->next;
             delete tmp;
         }
