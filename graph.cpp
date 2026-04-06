89e522c975bd1ee99b75ef971e2636a170833508
sgatz1 <sgatz1@asu.edu>
2026-04-06 02:19:46 -0400

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -1,29 +1,22 @@
 #include "graph.h"
-#include <cfloat>  // for DBL_MAX
+#include <cstdio>
 
-// Relaxation for Dijkstra
-void relax(pVERTEX u, pNODE node, pVERTEX* vertices) {
-    if (node && u->key + node->w < node->v->key) {
-        node->v->key = u->key + node->w;
-        node->v->pi = u->index;
-    }
-}
-
-// Add edge to adjacency list
-void addEdge(pNODE* adjList, int u, int v, double w, pVERTEX* vertices) {
-    pNODE newNode = new NODE;
-    newNode->u = u;
-    newNode->v = vertices[v];
-    newNode->w = w;
-    newNode->next = adjList[u];
-    adjList[u] = newNode;
-}
-
-// Initialize adjacency list
 void initializeGraph(pVERTEX* vertices, pNODE* adjList, int n) {
-    for (int i = 0; i < n; i++) {
-        vertices[i]->key = DBL_MAX;
-        vertices[i]->pi = -1;
+    for (int i = 0; i < n; ++i) {
+        vertices[i] = new TAG_VERTEX{i, DBL_MAX, -1};
         adjList[i] = nullptr;
     }
 }
+
+void addEdge(pNODE* adjList, int u, int v, double w) {
+    pNODE newNode = new TAG_NODE{u, v, w, adjList[u]};
+    adjList[u] = newNode;
+}
+
+void relax(pVERTEX u, pNODE node, pVERTEX* vertices) {
+    if (node && u->key + node->w < vertices[node->v]->key) {
+        vertices[node->v]->key = u->key + node->w;
+        vertices[node->v]->pi = u->index;
+    }
+}
+
