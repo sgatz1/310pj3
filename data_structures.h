d66326e6c1a778b84e20bd6874923be396270d60
sgatz1 <sgatz1@asu.edu>
2026-04-06 02:54:40 -0400

    Update data_structures.h

--- data_structures.h
+++ data_structures.h
@@ -1,22 +1,49 @@
 #ifndef DATA_STRUCTURES_H
 #define DATA_STRUCTURES_H
 
-#include <cfloat>   // DBL_MAX
-#include <cstdlib>  // nullptr
+#include <cfloat>   // for DBL_MAX
+#include <cstdlib>  // for nullptr
 
-struct TAG_VERTEX {
-    int index;
+// Forward type definitions
+typedef struct TAG_VERTEX* pVERTEX;
+typedef struct TAG_NODE*   pNODE;
+typedef struct TAG_ELEMENT* pELEMENT;
+typedef struct TAG_HEAP*    pHEAP;
+typedef struct TAG_STACK*   pSTACK;
+
+// Vertex structure
+typedef struct TAG_VERTEX {
+    int index;        // vertex ID
+    double key;       // distance for Dijkstra
+    int pi;           // predecessor index
+} VERTEX;
+
+// Node structure (for adjacency list)
+typedef struct TAG_NODE {
+    int u;            // source vertex index
+    pVERTEX v;        // pointer to destination vertex
+    double w;         // edge weight
+    pNODE next;       // next node in adjacency list
+} NODE;
+
+// Heap element (for priority queue in Dijkstra)
+typedef struct TAG_ELEMENT {
+    pVERTEX vertex;
     double key;
-    int pi; // predecessor
-};
-typedef TAG_VERTEX* pVERTEX;
+} ELEMENT;
 
-struct TAG_NODE {
-    int u;
-    int v;
-    double w;
-    TAG_NODE* next;
-};
-typedef TAG_NODE* pNODE;
+// Heap structure
+typedef struct TAG_HEAP {
+    int size;        // current size
+    int capacity;    // max capacity
+    pELEMENT* arr;   // array of ELEMENT pointers
+} HEAP;
 
-#endif
+// Stack structure
+typedef struct TAG_STACK {
+    int top;
+    int capacity;
+    pELEMENT* arr;   // array of ELEMENT pointers
+} STACK;
+
+#endif // DATA_STRUCTURES_H
