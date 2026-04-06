b5750bdbdd2022dbefb2cc285a2ef167ee92fe49
sgatz1 <sgatz1@asu.edu>
2026-04-06 02:01:51 -0400

    Update util.cpp

--- util.cpp
+++ util.cpp
@@ -1,50 +1,60 @@
 #include "util.h"
+#include "data_structures.h"
+#include "heap.h"
 #include <iostream>
-#include <limits>
-#include <cstdlib>
+#include <cfloat>
 
-void initializeGraph(pVERTEX* vertices, pNODE** adjList, int n) {
-    for (int i = 0; i < n; i++) {
-        vertices[i]->index = i;
-        vertices[i]->color = WHITE;
-        vertices[i]->key = std::numeric_limits<double>::infinity();
-        vertices[i]->pi = -1;
-        vertices[i]->position = -1;
-        adjList[i] = nullptr;
-    }
-}
-
-void addEdge(pNODE** adjList, int u, int v, double w) {
-    pNODE newNode = new NODE{0, u, v, w, adjList[u]};
+// add an edge to the adjacency list
+void addEdge(pNODE* adjList, int u, int v, double w) {
+    pNODE newNode = new NODE;
+    newNode->index = 0;  // optional, can be ignored
+    newNode->u = u;
+    newNode->v = v;
+    newNode->w = w;
+    newNode->next = adjList[u];
     adjList[u] = newNode;
 }
 
-void dijkstra(pVERTEX* vertices, pNODE** adjList, int startIndex, int n) {
-    HEAP* H = createHeap(n);
-
+// initialize single source
+void initializeSingleSource(pVERTEX* vertices, int n, int startIndex) {
     for (int i = 0; i < n; i++) {
-        vertices[i]->key = std::numeric_limits<double>::infinity();
-        insertHeap(H, vertices[i]);
+        vertices[i]->key = DBL_MAX;
+        vertices[i]->pi = -1;
+        vertices[i]->color = WHITE;
     }
+    vertices[startIndex]->key = 0;
+}
 
-    vertices[startIndex]->key = 0.0;
-    decreaseKey(H, startIndex, 0.0);
+// relax edge
+void relax(pVERTEX u, pNODE node, pVERTEX* vertices) {
+    pVERTEX v = vertices[node->v];
+    if (v->key > u->key + node->w) {
+        v->key = u->key + node->w;
+        v->pi = u->index;
+    }
+}
+
+// Dijkstra algorithm
+void dijkstra(pVERTEX* vertices, pNODE* adjList, int startIndex, int n) {
+    initializeSingleSource(vertices, n, startIndex);
+
+    HEAP* H = createHeap(n);
+    for (int i = 0; i < n; i++) {
+        H->arr[i] = vertices[i];
+        H->arr[i]->position = i;
+    }
+    H->size = n;
 
     while (!isEmpty(H)) {
         pVERTEX u = extractMin(H);
         for (pNODE node = adjList[u->index]; node != nullptr; node = node->next) {
-            pVERTEX v = vertices[node->v];
-            if (v->key > u->key + node->w) {
-                v->key = u->key + node->w;
-                v->pi = u->index;
-                decreaseKey(H, v->position, v->key);
-            }
+            relax(u, *node, vertices);
         }
     }
-
     freeHeap(H);
 }
 
+// print shortest path recursively
 void printShortestPath(pVERTEX* vertices, int startIndex, int endIndex) {
     if (endIndex == startIndex) {
         std::cout << startIndex;
@@ -53,5 +63,15 @@ void printShortestPath(pVERTEX* vertices, int startIndex, int endIndex) {
     } else {
         printShortestPath(vertices, startIndex, vertices[endIndex]->pi);
         std::cout << " -> " << endIndex;
+    }
+}
+
+// optional: initialize graph function for main
+void initializeGraph(pVERTEX* vertices, pNODE* adjList, int n) {
+    for (int i = 0; i < n; i++) {
+        vertices[i]->key = DBL_MAX;
+        vertices[i]->pi = -1;
+        vertices[i]->color = WHITE;
+        adjList[i] = nullptr;
     }
 }
