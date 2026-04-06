963a9cfdf78d08a381fc7d3dcd24d750936e779c
sgatz1 <sgatz1@asu.edu>
2026-04-06 02:59:50 -0400

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,13 +1,28 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-#include "data_structures.h"
+#include <iostream>
+#include <cfloat>
 
-// Graph operations
-void initializeGraph(pVERTEX* vertices, pNODE* adjList[], int n);
-void addEdge(pNODE* adjList[], int u, pVERTEX v, double w);
-void relax(pVERTEX u, pNODE node);
-void dijkstra(pVERTEX* vertices, pNODE* adjList[], int startIndex, int n);
-void printShortestPath(pVERTEX* vertices, int startIndex, int endIndex);
+struct TAG_VERTEX {
+    int index;
+    double key;
+    int pi; // predecessor
+};
+typedef TAG_VERTEX* pVERTEX;
+
+struct TAG_NODE {
+    pVERTEX v;      // pointer to vertex
+    double w;       // weight
+    TAG_NODE* next; // next node in adjacency list
+};
+typedef TAG_NODE* pNODE;
+
+// Graph functions
+void initializeGraph(pVERTEX vertices[], pNODE adjList[], int n);
+void addEdge(pNODE adjList[], int u, pVERTEX v, double w);
+void dijkstra(pVERTEX vertices[], pNODE adjList[], int startIndex, int n);
+void relax(pVERTEX u, pNODE node, pVERTEX vertices[]);
+void printShortestPath(pVERTEX vertices[], int startIndex, int endIndex);
 
 #endif
