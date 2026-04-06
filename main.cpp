4649f5e1acfd48c8446be277ef86782819d41019
sgatz1 <sgatz1@asu.edu>
2026-04-06 03:00:56 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
+#include <cfloat>
 #include "graph.h"
-#include "data_structures.h"
+using namespace std;
 
 int main() {
-    const int n = 5;  // number of vertices
+    const int n = 5; // number of vertices
     pVERTEX vertices[n];
-    pNODE* adjList[n];
+    pNODE adjList[n];
 
     initializeGraph(vertices, adjList, n);
 
-    // Add edges
+    // Add edges (example)
     addEdge(adjList, 0, vertices[1], 2.0);
     addEdge(adjList, 0, vertices[2], 4.0);
     addEdge(adjList, 1, vertices[2], 1.0);
@@ -17,25 +18,23 @@ int main() {
     addEdge(adjList, 2, vertices[4], 3.0);
     addEdge(adjList, 3, vertices[4], 1.0);
 
-    dijkstra(vertices, adjList, 0, n);
-
     // Print adjacency list
     for (int i = 0; i < n; i++) {
-        std::cout << "ADJ[" << i << "]:";
-        for (pNODE curr = adjList[i]; curr != nullptr; curr = curr->next) {
-            std::cout << "-->[" << curr->u << " " << curr->v->index
-                      << ": " << curr->w << "]";
+        cout << "ADJ[" << i << "]:";
+        pNODE curr = adjList[i];
+        while (curr) {
+            cout << "-->[" << i << " " << curr->v->index << ": " << curr->w << "]";
+            curr = curr->next;
         }
-        std::cout << "\n";
+        cout << endl;
     }
 
-    // Print shortest paths from vertex 0
-    std::cout << "\nShortest paths from 0:\n";
-    for (int i = 0; i < n; i++) {
-        std::cout << "Path to " << i << ": ";
-        printShortestPath(vertices, 0, i);
-        std::cout << "\n";
-    }
+    // Run Dijkstra from vertex 0
+    dijkstra(vertices, adjList, 0, n);
+
+    // Print shortest path to vertex 4
+    cout << "Shortest path from 0 to 4: ";
+    printShortestPath(vertices, 0, 4);
+    cout << endl;
 
     return 0;
-}
