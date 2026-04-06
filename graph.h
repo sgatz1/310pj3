24d976eba6ab7b4d95d36c24caa1a06b4881ae17
sgatz1 <sgatz1@asu.edu>
2026-04-06 02:31:06 -0400

    Update graph.h

--- graph.h
+++ graph.h
@@ -13,22 +13,20 @@ typedef TAG_NODE* pNODE;
 
 struct TAG_VERTEX {
     int index;
-    double key;   // For Dijkstra
-    int pi;       // predecessor
+    double key;
+    int pi;  // predecessor
 };
 
 struct TAG_NODE {
-    int u;
-    pVERTEX v;
-    double w;
-    pNODE next;
+    int u;        // source index
+    pVERTEX v;    // pointer to destination vertex
+    double w;     // edge weight
+    TAG_NODE* next;
 };
 
+// Graph functions
 void initializeGraph(pVERTEX* vertices, pNODE* adjList[], int n);
 void addEdge(pNODE* adjList[], int u, pVERTEX v, double w);
-void printGraph(pNODE* adjList[], int n);
-
-// Dijkstra utilities
 void relax(pVERTEX u, pNODE node);
 void dijkstra(pVERTEX* vertices, pNODE* adjList[], int startIndex, int n);
 void printShortestPath(pVERTEX* vertices, int startIndex, int endIndex);
