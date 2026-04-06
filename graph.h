37f33e9587eccd808b9bb22054764aeda8cc977d
sgatz1 <sgatz1@asu.edu>
2026-04-06 02:37:51 -0400

    Update graph.h

--- graph.h
+++ graph.h
@@ -1,9 +1,8 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
-#include <cfloat>
-#include <cstdio>
-#include <cstdlib>
+#include <cfloat>   // for DBL_MAX
+#include <iostream>
 
 struct TAG_VERTEX;
 struct TAG_NODE;
@@ -14,21 +13,19 @@ typedef TAG_NODE* pNODE;
 struct TAG_VERTEX {
     int index;
     double key;
-    int pi;  // predecessor
+    int pi;
 };
 
 struct TAG_NODE {
-    int u;        // source index
-    pVERTEX v;    // pointer to destination vertex
-    double w;     // edge weight
+    int u;
+    pVERTEX v;
+    double w;
     TAG_NODE* next;
 };
 
-// Graph functions
 void initializeGraph(pVERTEX* vertices, pNODE* adjList[], int n);
 void addEdge(pNODE* adjList[], int u, pVERTEX v, double w);
-void relax(pVERTEX u, pNODE node);
 void dijkstra(pVERTEX* vertices, pNODE* adjList[], int startIndex, int n);
-void printShortestPath(pVERTEX* vertices, int startIndex, int endIndex);
+void relax(pVERTEX u, pNODE node);
 
 #endif
