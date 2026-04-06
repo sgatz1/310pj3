cdbcf37b76224ae0ea30470d46c26f89ea74d021
sgatz1 <sgatz1@asu.edu>
2026-04-06 02:15:29 -0400

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -1,73 +1,29 @@
 #include "graph.h"
-#include <iostream>
-#include <limits>
-#include <queue>
+#include <cfloat>  // for DBL_MAX
 
-// Initialize graph vertices and adjacency list
-void initializeGraph(pVERTEX* vertices, pNODE* adjList, int n) {
-    for (int i = 0; i < n; i++) {
-        vertices[i]->color = WHITE;
-        vertices[i]->key = std::numeric_limits<double>::infinity();
-        vertices[i]->pi = -1;
-        adjList[i] = nullptr;  // no edges yet
-    }
-}
-
-// Add edge to adjacency list
-void addEdge(pNODE* adjList, int u, int v, double w) {
-    pNODE node = new NODE;
-    node->u = u;
-    node->v = v;
-    node->w = w;
-    node->next = adjList[u];
-    adjList[u] = node;
-}
-
-// Relax edge u->v
-void relax(pVERTEX u, pNODE node) {
+// Relaxation for Dijkstra
+void relax(pVERTEX u, pNODE node, pVERTEX* vertices) {
     if (node && u->key + node->w < node->v->key) {
         node->v->key = u->key + node->w;
         node->v->pi = u->index;
     }
 }
 
-// Dijkstra algorithm
-void dijkstra(pVERTEX* vertices, pNODE* adjList, int startIndex, int n) {
-    vertices[startIndex]->key = 0;
-    
-    // Simple priority queue using pair<key, index>
-    std::priority_queue<std::pair<double,int>,
-                        std::vector<std::pair<double,int>>,
-                        std::greater<std::pair<double,int>>> pq;
-    
-    for (int i = 0; i < n; i++)
-        pq.push({vertices[i]->key, i});
-
-    while (!pq.empty()) {
-        int u_index = pq.top().second;
-        pq.pop();
-
-        pNODE node = adjList[u_index];
-        while (node) {
-            int v_index = node->v;
-            if (vertices[u_index]->key + node->w < vertices[v_index]->key) {
-                vertices[v_index]->key = vertices[u_index]->key + node->w;
-                vertices[v_index]->pi = u_index;
-                pq.push({vertices[v_index]->key, v_index});
-            }
-            node = node->next;
-        }
-    }
+// Add edge to adjacency list
+void addEdge(pNODE* adjList, int u, int v, double w, pVERTEX* vertices) {
+    pNODE newNode = new NODE;
+    newNode->u = u;
+    newNode->v = vertices[v];
+    newNode->w = w;
+    newNode->next = adjList[u];
+    adjList[u] = newNode;
 }
 
-// Print shortest path from start to end
-void printShortestPath(pVERTEX* vertices, int startIndex, int endIndex) {
-    if (endIndex == startIndex) {
-        std::cout << startIndex;
-    } else if (vertices[endIndex]->pi == -1) {
-        std::cout << "No path";
-    } else {
-        printShortestPath(vertices, startIndex, vertices[endIndex]->pi);
-        std::cout << " -> " << endIndex;
+// Initialize adjacency list
+void initializeGraph(pVERTEX* vertices, pNODE* adjList, int n) {
+    for (int i = 0; i < n; i++) {
+        vertices[i]->key = DBL_MAX;
+        vertices[i]->pi = -1;
+        adjList[i] = nullptr;
     }
 }
