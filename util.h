9056f7927d58f9eb23e82f635674a2e514f3fc49
sgatz1 <sgatz1@asu.edu>
2026-04-06 01:53:13 -0400

    Add graph and Dijkstra functions to util.h

--- util.h
+++ util.h
@@ -3,10 +3,16 @@
 #define UTIL_H
 
 #include "data_structures.h"
+#include "heap.h"
 
-// Original utility functions
-void printVertex(pVERTEX v);
-void printNode(pNODE node);
-void freeAdjList(pNODE* adjList, int n);
+// Graph functions
+void initializeGraph(pVERTEX* vertices, pNODE** adjList, int n);
+void addEdge(pNODE** adjList, int u, int v, double w);
+
+// Dijkstra
+void dijkstra(pVERTEX* vertices, pNODE** adjList, int startIndex, int n);
+
+// Printing
+void printShortestPath(pVERTEX* vertices, int startIndex, int endIndex);
 
 #endif // UTIL_H
