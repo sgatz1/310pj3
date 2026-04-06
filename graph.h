4d16cbc4a202463d2518c500778b522a65533302
sgatz1 <sgatz1@asu.edu>
2026-04-06 02:19:32 -0400

    Refactor graph.h to include relax function

--- graph.h
+++ graph.h
@@ -1,19 +1,12 @@
-#pragma once
 #ifndef GRAPH_H
 #define GRAPH_H
 
 #include "data_structures.h"
 
-// initialize graph
 void initializeGraph(pVERTEX* vertices, pNODE* adjList, int n);
-
-// add edge
 void addEdge(pNODE* adjList, int u, int v, double w);
-
-// dijkstra algorithm
+void relax(pVERTEX u, pNODE node, pVERTEX* vertices);
 void dijkstra(pVERTEX* vertices, pNODE* adjList, int startIndex, int n);
-
-// print shortest path
 void printShortestPath(pVERTEX* vertices, int startIndex, int endIndex);
 
-#endif // GRAPH_H
+#endif
