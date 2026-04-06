570c66a9d55aca90fbc346aa7a7429c61280cbdb
sgatz1 <sgatz1@asu.edu>
2026-04-06 01:42:21 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include "data_structures.h"
 #include "graph.h"
-#include "util.h"
 
 int main() {
-    int n = 5; // example number of vertices
-    pVERTEX* vertices;
-    pNODE* adjList;
+    int n = 5;
+    // allocate vertex array
+    pVERTEX* vertices = new pVERTEX[n];
+    for (int i = 0; i < n; ++i)
+        vertices[i] = new VERTEX;
 
+    // allocate adjacency list (array of pNODE)
+    pNODE* adjList = new pNODE[n]; // this is pNODE** when passed
+     
     initializeGraph(vertices, adjList, n);
 
+    // add edges
     addEdge(adjList, 0, 1, 2.0);
     addEdge(adjList, 0, 2, 4.0);
     addEdge(adjList, 1, 2, 1.0);
@@ -17,20 +22,20 @@ int main() {
     addEdge(adjList, 2, 4, 3.0);
     addEdge(adjList, 3, 4, 1.0);
 
+    // run dijkstra from vertex 0
     dijkstra(vertices, adjList, 0, n);
 
-    for (int i = 0; i < n; i++) {
-        std::cout << "Shortest path to " << i << ": ";
-        printShortestPath(vertices, 0, i);
-    }
+    // print shortest path to vertex 4
+    printShortestPath(vertices, 0, 4);
+    std::cout << std::endl;
 
-    // Free memory
-    for (int i = 0; i < n; i++) {
+    // cleanup
+    for (int i = 0; i < n; ++i) {
         delete vertices[i];
-        pNODE curr = adjList[i];
-        while (curr) {
-            pNODE temp = curr;
-            curr = curr->next;
+        pNODE node = adjList[i];
+        while (node) {
+            pNODE temp = node;
+            node = node->next;
             delete temp;
         }
     }
