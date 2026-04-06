05d6202536c24453f10e9cd62520b1efa24e3e10
sgatz1 <sgatz1@asu.edu>
2026-04-06 02:25:44 -0400

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,23 +1,20 @@
-#pragma once
 #ifndef STACK_H
 #define STACK_H
 
-#include "data_structures.h" // ensures TAG_VERTEX is defined
+struct TAG_ELEMENT;
+typedef TAG_ELEMENT* pELEMENT;
 
-// define stack element as VERTEX pointer
-typedef TAG_VERTEX ELEMENT;
-typedef ELEMENT* pELEMENT;
-
-typedef struct TAG_STACK {
+struct TAG_STACK {
     int top;
     int capacity;
-    pELEMENT* arr;  // array of ELEMENT pointers
-} STACK, *pSTACK;
+    pELEMENT* arr;
+};
+
+typedef TAG_STACK* pSTACK;
 
 pSTACK createStack(int capacity);
 void push(pSTACK S, pELEMENT item);
 pELEMENT pop(pSTACK S);
-bool isEmptyStack(pSTACK S);
 void freeStack(pSTACK S);
 
-#endif // STACK_H
+#endif
