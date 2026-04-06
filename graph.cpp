39da44fc708c0e76940169c18d7c296469a10ca3
sgatz1 <sgatz1@asu.edu>
2026-04-06 03:00:17 -0400

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -1,20 +1,18 @@
 #include "graph.h"
-#include <cstdio>
-#include <cfloat>
+#include <iostream>
+using namespace std;
 
-// Initialize vertices and adjacency list
-void initializeGraph(pVERTEX* vertices, pNODE* adjList[], int n) {
+void initializeGraph(pVERTEX vertices[], pNODE adjList[], int n) {
     for (int i = 0; i < n; i++) {
-        vertices[i] = new VERTEX{i, DBL_MAX, -1};
-        adjList[i] = nullptr;  // empty adjacency list
+        vertices[i] = new TAG_VERTEX{ i, DBL_MAX, -1 };
+        adjList[i] = nullptr;
     }
 }
 
-// Add an edge to adjacency list
-void addEdge(pNODE* adjList[], int u, pVERTEX v, double w) {
-    pNODE newNode = new NODE{u, v, w, nullptr};
+void addEdge(pNODE adjList[], int u, pVERTEX v, double w) {
+    pNODE newNode = new TAG_NODE{ v, w, nullptr };
 
-    // Insert at rear
+    // Insert at the end of adjacency list
     if (!adjList[u]) {
         adjList[u] = newNode;
     } else {
@@ -24,51 +22,48 @@ void addEdge(pNODE* adjList[], int u, pVERTEX v, double w) {
     }
 }
 
-// Relax operation for Dijkstra
-void relax(pVERTEX u, pNODE node) {
+void relax(pVERTEX u, pNODE node, pVERTEX vertices[]) {
     if (node && u->key + node->w < node->v->key) {
         node->v->key = u->key + node->w;
         node->v->pi = u->index;
     }
 }
 
-// Simple Dijkstra (no heap)
-void dijkstra(pVERTEX* vertices, pNODE* adjList[], int startIndex, int n) {
+void dijkstra(pVERTEX vertices[], pNODE adjList[], int startIndex, int n) {
     vertices[startIndex]->key = 0;
 
+    // Simple unoptimized Dijkstra
     bool visited[n];
     for (int i = 0; i < n; i++) visited[i] = false;
 
     for (int count = 0; count < n; count++) {
-        // Find vertex with minimum key not yet visited
+        // Find minimum key among unvisited
         double minKey = DBL_MAX;
         int uIndex = -1;
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
             if (!visited[i] && vertices[i]->key < minKey) {
                 minKey = vertices[i]->key;
                 uIndex = i;
             }
+        }
 
         if (uIndex == -1) break;
         visited[uIndex] = true;
-
-        // Relax all neighbors
-        for (pNODE node = adjList[uIndex]; node != nullptr; node = node->next) {
-            relax(vertices[uIndex], node);
+        pNODE node = adjList[uIndex];
+        while (node) {
+            relax(vertices[uIndex], node, vertices);
+            node = node->next;
         }
     }
 }
 
-// Print path from start to end
-void printShortestPath(pVERTEX* vertices, int startIndex, int endIndex) {
-    if (startIndex == endIndex) {
-        printf("%d ", startIndex);
-        return;
+void printShortestPath(pVERTEX vertices[], int startIndex, int endIndex) {
+    if (endIndex == startIndex) {
+        cout << startIndex << " ";
+    } else if (vertices[endIndex]->pi == -1) {
+        cout << "No path from " << startIndex << " to " << endIndex << endl;
+    } else {
+        printShortestPath(vertices, startIndex, vertices[endIndex]->pi);
+        cout << endIndex << " ";
     }
-    if (vertices[endIndex]->pi == -1) {
-        printf("No path from %d to %d\n", startIndex, endIndex);
-        return;
-    }
-    printShortestPath(vertices, startIndex, vertices[endIndex]->pi);
-    printf("%d ", endIndex);
 }
