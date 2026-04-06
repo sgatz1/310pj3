f22cdbdc362c4990c6f47f5254354393d82e6f68
sgatz1 <sgatz1@asu.edu>
2026-04-06 01:53:31 -0400

    Refactor Dijkstra's algorithm and related functions

--- util.cpp
+++ util.cpp
@@ -1,52 +1,57 @@
 #include "util.h"
-#include "heap.h"
 #include <iostream>
+#include <limits>
+#include <cstdlib>
 
-void initializeSingleSource(pVERTEX* vertices, int n, int s) {
+void initializeGraph(pVERTEX* vertices, pNODE** adjList, int n) {
     for (int i = 0; i < n; i++) {
-        vertices[i]->key = 1e9;
-        vertices[i]->pi = -1;
+        vertices[i]->index = i;
         vertices[i]->color = WHITE;
-    }
-    vertices[s]->key = 0;
-}
-
-void relax(VERTEX* u, VERTEX* v, double w) {
-    if (v->key > u->key + w) {
-        v->key = u->key + w;
-        v->pi = u->index;
+        vertices[i]->key = std::numeric_limits<double>::infinity();
+        vertices[i]->pi = -1;
+        vertices[i]->position = -1;
+        adjList[i] = nullptr;
     }
 }
 
-void dijkstra(pVERTEX* vertices, pNODE* adjList, int s, int n) {
-    initializeSingleSource(vertices, n, s);
+void addEdge(pNODE** adjList, int u, int v, double w) {
+    pNODE newNode = new NODE{0, u, v, w, adjList[u]};
+    adjList[u] = newNode;
+}
 
-    pHEAP H = createHeap(n);
+void dijkstra(pVERTEX* vertices, pNODE** adjList, int startIndex, int n) {
+    HEAP* H = createHeap(n);
+
     for (int i = 0; i < n; i++) {
-        H->arr[i] = vertices[i];
-        H->size++;
+        vertices[i]->key = std::numeric_limits<double>::infinity();
+        insertHeap(H, vertices[i]);
     }
+
+    vertices[startIndex]->key = 0.0;
+    decreaseKey(H, startIndex, 0.0);
 
     while (!isEmpty(H)) {
         pVERTEX u = extractMin(H);
-        pNODE edge = adjList[u->index];
-        while (edge != nullptr) {
-            relax(u, vertices[edge->v], edge->w);
-            edge = edge->next;
+        for (pNODE node = adjList[u->index]; node != nullptr; node = node->next) {
+            pVERTEX v = vertices[node->v];
+            if (v->key > u->key + node->w) {
+                v->key = u->key + node->w;
+                v->pi = u->index;
+                decreaseKey(H, v->position, v->key);
+            }
         }
     }
 
     freeHeap(H);
 }
 
-void printShortestPath(pVERTEX* vertices, int s, int v) {
-    if (v == s) {
-        std::cout << s;
-    } else if (vertices[v]->pi == -1) {
+void printShortestPath(pVERTEX* vertices, int startIndex, int endIndex) {
+    if (endIndex == startIndex) {
+        std::cout << startIndex;
+    } else if (vertices[endIndex]->pi == -1) {
         std::cout << "No path";
     } else {
-        printShortestPath(vertices, s, vertices[v]->pi);
-        std::cout << " -> " << v;
+        printShortestPath(vertices, startIndex, vertices[endIndex]->pi);
+        std::cout << " -> " << endIndex;
     }
-    std::cout << std::endl;
 }
