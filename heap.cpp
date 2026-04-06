9a9e3e19bd923bd72ebfd58354d5140181dc5fcd
sgatz1 <sgatz1@asu.edu>
2026-04-06 02:49:22 -0400

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -1,36 +1,63 @@
 #include "heap.h"
+#include "data_structures.h"  // Make sure TAG_ELEMENT is defined here
 #include <algorithm>
+#include <iostream>
+
+using namespace std;
 
 pHEAP createHeap(int capacity) {
-    pHEAP H = new TAG_HEAP{0, capacity, new pELEMENT[capacity]};
+    pHEAP H = new TAG_HEAP;
+    H->size = 0;
+    H->capacity = capacity;
+    H->arr = new pELEMENT[capacity];
     return H;
 }
 
-void insert(pHEAP H, pELEMENT item) {
-    if (H->size >= H->capacity) return;
+void swap(pELEMENT &a, pELEMENT &b) {
+    pELEMENT temp = a;
+    a = b;
+    b = temp;
+}
+
+void heapify(pHEAP H, int i) {
+    int smallest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+
+    if (l < H->size && H->arr[l]->key < H->arr[smallest]->key)
+        smallest = l;
+    if (r < H->size && H->arr[r]->key < H->arr[smallest]->key)
+        smallest = r;
+
+    if (smallest != i) {
+        swap(H->arr[i], H->arr[smallest]);
+        heapify(H, smallest);
+    }
+}
+
+void insert(pHEAP H, pELEMENT elem) {
+    if (H->size == H->capacity) {
+        cout << "Heap full!\n";
+        return;
+    }
+
     int i = H->size++;
-    H->arr[i] = item;
+    H->arr[i] = elem;
+
     while (i != 0 && H->arr[(i - 1) / 2]->key > H->arr[i]->key) {
-        std::swap(H->arr[i], H->arr[(i - 1) / 2]);
+        swap(H->arr[i], H->arr[(i - 1) / 2]);
         i = (i - 1) / 2;
     }
 }
 
 pELEMENT extractMin(pHEAP H) {
     if (H->size <= 0) return nullptr;
+    if (H->size == 1) return H->arr[--H->size];
+
     pELEMENT root = H->arr[0];
     H->arr[0] = H->arr[--H->size];
-    int i = 0;
-    while (true) {
-        int l = 2 * i + 1, r = 2 * i + 2;
-        int smallest = i;
-        if (l < H->size && H->arr[l]->key < H->arr[smallest]->key) smallest = l;
-        if (r < H->size && H->arr[r]->key < H->arr[smallest]->key) smallest = r;
-        if (smallest != i) {
-            std::swap(H->arr[i], H->arr[smallest]);
-            i = smallest;
-        } else break;
-    }
+    heapify(H, 0);
+
     return root;
 }
 
